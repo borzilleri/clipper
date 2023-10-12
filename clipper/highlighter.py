cbb7c7a7c6b17db1f9590cf54a885161c70a1cad
Jonathan Borzilleri <jonathan@borzilleri.net>
2023-10-11 22:16:50 -0700

    Handle bad syntax name when finding lexer

--- clipper/highlighter.py
+++ clipper/highlighter.py
@@ -3,6 +3,7 @@ from .util import is_fenced, get_fences
 from typing import Optional
 from pathlib import Path
 import pygments, pygments.lexers, pygments.formatters, pygments.styles
+from pygments.util import ClassNotFound
 
 DEFAULT_THEME = "monokai"
 
@@ -11,11 +12,17 @@ def syntax_from_extension(filename) -> Optional[str]:
     exts = [ext.strip('.') for ext in Path(filename).suffixes[:-1]]
     return languages.ext_to_lang(exts[-1]) if exts else None
 
-def highlight_pygments(text: str, syntax, theme: str) -> str:
+def highlight_pygments(text: str, syntax: Optional[str], theme: str) -> str:
+    lexer = None
     if syntax:
-        lexer = pygments.lexers.get_lexer_by_name(syntax)
+        try:
+            lexer = pygments.lexers.get_lexer_by_name(syntax)
+        except ClassNotFound:
+            pass
     else:
         lexer = pygments.lexers.guess_lexer(text)
+    if not lexer:
+        lexer = pygments.lexers.get_lexer_by_name('text')
     style = pygments.styles.get_style_by_name(theme)
     formatter = pygments.formatters.get_formatter_by_name('terminal16m', style=style)
     return pygments.highlight(text, lexer=lexer, formatter=formatter)

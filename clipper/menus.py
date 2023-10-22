c8d7b99a26d747837b85fefe8a56538909a9b852
Jonathan Borzilleri <jonathan@borzilleri.net>
2023-10-22 14:34:56 -0700

    Minor code cleanup of menus module

--- clipper/menus.py
+++ clipper/menus.py
@@ -6,10 +6,11 @@ import regex as re
 import shlex
 import shutil
 
-MENUS = ["fzf", "gum"]
+__DEFAULT_MENU = "console"
+__MENUS = ["fzf", "gum"]
 
 
-def find_query_in_options(data: list, filename: str, query: str) -> list:
+def __find_query_in_options(data: list, filename: str, query: str) -> list:
     options = [f"{filename} {x['title']}" for x in data]
     words = re.split(r" +", query)
     words = list(
@@ -24,8 +25,8 @@ def find_query_in_options(data: list, filename: str, query: str) -> list:
     return [w.lower() for w in words]
 
 
-def remove_items_without_query(data: list, filename: str, query: str):
-    q = find_query_in_options(data, filename, query)
+def __remove_items_without_query(data: list, filename: str, query: str):
+    q = __find_query_in_options(data, filename, query)
 
     result = list(
         filter(
@@ -44,35 +45,39 @@ def remove_items_without_query(data: list, filename: str, query: str):
     return result
 
 
-def best_menu(selection: Optional[str]):
+def __menu_exists(menu: str) -> bool:
+    return bool(menu == __DEFAULT_MENU or shutil.which(menu))
+
+
+def __best_menu(selection: Optional[str]):
     # If the selection exists, use it
-    if selection and (selection == "console" or shutil.which(selection)):
+    if selection and __menu_exists(selection):
         return selection
     # Otherwise, look for the first available option
-    for m in MENUS:
+    for m in __MENUS:
         if shutil.which(m):
             return m
     # or default to console.
-    return "console"
+    return __DEFAULT_MENU
 
 
-def menu_gum(
+def __menu_gum(
     input: list, title: str, filename: Optional[str] = None, query: Optional[str] = None
 ):
     raise NotImplementedError
 
 
-def menu_fzf(
+def __menu_fzf(
     input: list, title: str, filename: Optional[str] = None, query: Optional[str] = None
 ):
     raise NotImplementedError
 
 
-def menu_pick(
+def __menu_pick(
     data: list, title: str, filename: Optional[str] = None, query: Optional[str] = None
 ) -> Optional[Dict[str, str]]:
     if query and len(query) > 0:
-        data = remove_items_without_query(data, filename or "", query)
+        data = __remove_items_without_query(data, filename or "", query)
         if len(data) == 1:
             return data[0]
 
@@ -89,14 +94,14 @@ def menu_pick(
 def menu(
     data: list, title: str, filename: Optional[str] = None, query: Optional[str] = None
 ) -> Optional[Dict[str, str]]:
-    menu = best_menu(CONFIG.menus)
+    menu = __best_menu(CONFIG.menus)
     if query:
         words = list(filter(lambda w: re.search(r"^\w:", w), shlex.split(query)))
         query = " ".join(words).strip()
 
     if menu == "gum":
-        menu_gum(data, title, filename, query)
+        __menu_gum(data, title, filename, query)
     elif menu == "fzf":
-        menu_fzf(data, title, filename, query)
+        __menu_fzf(data, title, filename, query)
     else:
-        return menu_pick(data, title, filename, query)
+        return __menu_pick(data, title, filename, query)
